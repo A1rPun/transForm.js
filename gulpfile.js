9cfb75a26905be97952b001c1a97a212f6b46c90
A1rPun <thombakker@gmail.com>
2017-08-29 22:34:21 +0200

    :construction_worker: Add build task

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,24 @@ var rename = require('gulp-rename');
 var size = require('gulp-size');
 var uglify = require('gulp-uglify');
 
+var paths = {
+    transForm: './src/*.js',
+    dist: 'dist',
+};
+
 gulp.task('minify', function () {
-    return gulp.src('./src/transForm.js')
+    return gulp.src(paths.transForm)
         .pipe(size())
         .pipe(uglify())
         .pipe(size())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest(paths.dist))
 });
 
 gulp.task('lint', function () {
-    return gulp.src('./src/*.js')
+    return gulp.src(paths.transForm)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
+
+gulp.task('build', ['lint', 'minify']);
